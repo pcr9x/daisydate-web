d6653df31104e6faf47211bdc71d8cf1b5419f1f
newbieeiei1234 <tawan.yimratta@gmail.com>
2024-04-01 08:17:45 -0700

    chat, message page frontend, new message api

--- backend/api/chatting.py
+++ backend/api/chatting.py
@@ -1,15 +1,18 @@
 from fastapi import APIRouter, HTTPException, Request
+from starlette.responses import HTMLResponse
 from models.chatting import Message
 from api.suggested import chatting
 import transaction
 from fastapi.templating import Jinja2Templates
+from pydantic import BaseModel
+from api import users
 
 router = APIRouter()
 
 templates = Jinja2Templates(directory="../frontend/pages")
 
 
-@router.post("/messages/{chat_id}")
+@router.post("/sendMessage/{chat_id}")
 async def send_message(chat_id: str, message: Message):
     # Check if the chat exists
     chat = chatting.get(chat_id)
@@ -34,39 +37,79 @@ async def send_message(chat_id: str, message: Message):
     return {"message": "Message sent successfully"}
 
 
-@router.get("/messages/{chat_id}")
-async def get_messages(chat_id: str):
-    chat = chatting.get(chat_id)
-    if chat is None:
-        raise HTTPException(status_code=404, detail="Chat not found")
+# @router.get("/messages/{chat_id}")
+# async def get_messages(chat_id: str):
+#     chat = chatting.get(chat_id)
+#     if chat is None:
+#         raise HTTPException(status_code=404, detail="Chat not found")
 
-    return chat.message
+#     return chat.message
 
 
-@router.get("/messages/{user_id}/all")
-async def getAllChatIdByUser(user_id: str):
-    chat_ids = []
-    for key, value in chatting.items():
-        if value.userID1 == user_id or value.userID2 == user_id:
-            chat_ids.append(key)
-    return {"chat_ids": chat_ids}
+# @router.get("/messages/{user_id}/all")
+# async def getAllChatIdByUser(user_id: str):
+#     chat_ids = []
+#     for key, value in chatting.items():
+#         if value.userID1 == user_id or value.userID2 == user_id:
+#             chat_ids.append(key)
+#     return {"chat_ids": chat_ids}
 
 
 @router.get("/messages/all/test/test")
 async def get_all_chat_id():
     return list(chatting.values())
 
+class ChatResponse(BaseModel):
+    otherUserProfile: str
+    otherUserName: str
+    latestMessage: str
+    user_id: str
+    chatID: str
+    messages: list[Message]
 
 def get_all_chats(user_id: str):
     results = []
     for c in chatting:
         chat = chatting[c]
         if chat.userID1 == user_id or chat.userID2 == user_id:
-            results.append(chat)
-    print(results)
+            otherUser = None
+            if chat.userID1 == user_id:
+                otherUser = users.root[chat.userID2]
+            else:
+                otherUser = users.root[chat.userID1]
+            latesMessage = "" if len(chat.message) == 0 else chat.message[-1]
+            chatResponse = ChatResponse(
+                otherUserProfile= "", # add path to other user profile here
+                otherUserName=otherUser.name,
+                latestMessage=latesMessage,
+                chatID=chat.chatID,
+                user_id=user_id,
+                messages=chat.message
+            )
+            results.append(chatResponse)
     return results
 
-
+def get_chat_room(chat_id: str, user_id: str):
+    if chat_id not in chatting:
+        raise HTTPException(status_code=404, detail="Chat room not found")
+    chat = chatting[chat_id]
+    otherUser = None
+    if chat.userID1 == user_id:
+        otherUser = users.root[chat.userID2]
+    else:
+        otherUser = users.root[chat.userID1]
+    latesMessage = "" if len(chat.message) == 0 else chat.message[-1]
+    print(chat.message)
+    chatResponse = ChatResponse(
+        otherUserProfile= "", # add path to other user profile here
+        otherUserName=otherUser.name,
+        latestMessage="",
+        user_id= user_id,
+        chatID=chat.chatID,
+        messages=chat.message
+    )
+    return chatResponse
+    
 @router.get("/messages/{user_id}")
 async def index(user_id: str, request: Request):
     if user_id == "all":
@@ -74,4 +117,11 @@ async def index(user_id: str, request: Request):
     data = get_all_chats(user_id)
     return templates.TemplateResponse(
         "chat-page.html", {"request": request, "data": data}
+    )
+
+@router.get("/messages/{user_id}/{chat_id}")
+async def display_chat_room(user_id: str ,chat_id:str, request:Request):
+    data = get_chat_room(chat_id, user_id)
+    return templates.TemplateResponse(
+        "message-page.html", {"request": request, "data": data}
     )
